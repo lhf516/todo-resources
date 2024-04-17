d8a70d00c6b413142d7c237d76ab0510a37d2073
Lam Hoi Fung <lhf516@gmail.com>
2024-04-17 12:19:30 +0800

    Added MongoDB to resources

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # todo-resources
 Resources for the Todo List Sample Project
+
+## Prerequisites
+1. Create network manually
+   1. Network named `todo-sample` as an example
+2. Create a empty directory named `mongodb`
+3. Move `.env.sample` to `.env` and use your own configuration
+4. Update `init-db/mongodb/mongo-init.js` to use your own configuration
+
---
+++ init-db/mongodb/mongo-init.js
@@ -0,0 +1,23 @@
+db.createUser({
+  user: "todolist",
+  pwd: "123456",
+  roles: [
+    {
+      role: "readWrite",
+      db: "todolist",
+    },
+  ],
+});
+
+db = new Mongo().getDB("todolist");
+
+db.createCollection('users', { capped: false });
+db.createCollection('todos', { capped: false });
+
+// db.test.insert([
+//   { "item": 1 },
+//   { "item": 2 },
+//   { "item": 3 },
+//   { "item": 4 },
+//   { "item": 5 }
+// ]);
